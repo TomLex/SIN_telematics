ce7d3a6777cd2f97437327301f6abea5ba54381e
Tomi <tomi@robot.(none)>
2014-12-02 22:13:27 +0100

    modified run script

--- RUN.sh
+++ RUN.sh
@@ -1,1 +1,3 @@
-java -jar renew2.4.1/loader.jar setFormalism Timed Java Compiler --- gui --- demonstrator /renew2.4.1/samples/telematics/telematics.rnw
+PATH_TO_LOADER=renew2.4.1
+
+java -jar $PATH_TO_LOADER/loader.jar setFormalism Timed Java Compiler --- gui --- demonstrator $PATH_TO_LOADER/samples/telematics/telematics.rnw
